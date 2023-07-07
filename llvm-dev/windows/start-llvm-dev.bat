50d468258e97c05f1ab73066e933ff4b5af3e707
Muhammad Omair Javaid <omair.javaid@linaro.org>
2023-07-08 02:53:36 +0500

    Fix environment config on x64

--- llvm-dev/windows/start-llvm-dev.bat
+++ llvm-dev/windows/start-llvm-dev.bat
@@ -8,8 +8,12 @@ IF "%1" NEQ "amd64" IF "%1" NEQ "arm64" (
 
 :: set CUSTOM_PYHOME=C:\Users\%USERNAME%\scoop\apps\python\current
 :: set CUSTOM_PYHOME=C:\Users\omair\AppData\Local\Programs\Python\Python311-arm64
+IF "%1" EQU "amd64" (
+    set "DEBUG_DEPS=c:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\ucrt"
+) else (
+    set "DEBUG_DEPS=c:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\arm64\ucrt"
+)
 
-Set DEBUG_DEPS=c:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\%1\ucrt
 IF NOT EXIST "%DEBUG_DEPS%" (
     echo "Invalid path %DEBUG_DEPS%"
     EXIT /B 0
@@ -39,12 +43,8 @@ set LIT_OPTS="-svj 4"
 
 IF "%1"=="arm64" (
     "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsarm64.bat"
-) else (
-    echo Environment initialization for %1 Failed...
-)
-
-IF "%1"=="amd64" (
-    "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsamd64.bat"
+) else IF "%1"=="amd64" (
+    "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 ) else (
     echo Environment initialization for %1 Failed...
 )
