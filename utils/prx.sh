0eedcf1d48640f09f379c41a51912569f5114cec
Muhammad Omair Javaid <omair.javaid@linaro.org>
2023-11-20 15:59:33 +0500

    Add mac support to utils/prx.sh

--- utils/prx.sh
+++ utils/prx.sh
@@ -1,1 +1,16 @@
-/usr/bin/chromium-browser --incognito --proxy-server="socks5://127.0.0.1:5858"
+#!/bin/bash
+
+# Check the OS type
+ostype=$(uname -s)
+
+# Check if the OS is Linux
+if [[ $ostype == "Linux" ]]; then
+  brave-browser --incognito --proxy-server="socks5://127.0.0.1:5858"
+else
+  # Check if the OS is Mac
+  if [[ $ostype == "Darwin" ]]; then
+    open -a Brave\ Browser -n --args --incognito --proxy-server="socks5://127.0.0.1:5858"    
+  else
+    echo "Unknown OS"
+  fi
+fi
