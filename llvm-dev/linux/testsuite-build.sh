9d88d54e4a9980a6c2fc4f0e4b37effd880c2e39
Muhammad Omair Javaid <omair.javaid@linaro.org>
2023-12-20 17:19:23 +0500

    Fix build of Fortran + Flang LLVM Testsuite on linux

--- llvm-dev/linux/testsuite-build.sh
+++ llvm-dev/linux/testsuite-build.sh
@@ -2,11 +2,11 @@
 
 export CC=$HOME/work/llvm-dev/build/bin/clang-cl
 export CXX=$HOME/work/llvm-dev/build/bin/clang-cl
-export FF=$HOME/work/llvm-dev/build/bin/flang-new
+export FC=$HOME/work/llvm-dev/build/bin/flang-new
 
-export NO_STOP_MESSAGE=1
+#set NO_STOP_MESSAGE to 1 before running ninja check
 
-cmake -G Ninja -DTEST_SUITE_FORTRAN=ON -DCMAKE_Fortran_COMPILER=$FF -DTEST_SUITE_COLLECT_COMPILE_TIME=ON -DTEST_SUITE_LIT=/work/llvm-dev/build/bin/llvm-lit.py -DTEST_SUITE_SUBDIRS="Fortran" ../llvm-test-suite
+cmake -G Ninja -DTEST_SUITE_FORTRAN=ON -DTEST_SUITE_COLLECT_CODE_SIZE=OFF -DTEST_SUITE_LIT=$HOME/work/llvm-dev/build/bin/llvm-lit.py -DTEST_SUITE_SUBDIRS="Fortran" ../llvm-test-suite
 
 # Uncomment the following line if needed
 # cmake -G Ninja -DCMAKE_C_COMPILER="/work/llvm-dev/build/bin/clang-cl" -DTEST_SUITE_COLLECT_COMPILE_TIME=OFF -DTEST_SUITE_LIT=/work/llvm-dev/build/bin/llvm-lit.py -DTEST_SUITE_SUBDIRS="SingleSource" ../llvm-test-suite
